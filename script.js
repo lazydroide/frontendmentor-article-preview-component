fc4754fcc213ba47b02bb4b7bf8d61d474f0c428
lazydroide <lazydroide@gmail.com>
2024-10-23 13:12:36 +0200

    Project completed

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 const modal = document.getElementById('modal');
 const shareButton = document.getElementById('share-button');
-const icon = document.getElementById('share-icon');
-console.log(icon);
 
-shareButton.addEventListener('click', () => {
+
+shareButton.addEventListener('click', (e) => { 
     modal.classList.toggle('hidden');
     shareButton.classList.toggle('pressed');
 });
